773c469a854aa080752431c03f1cef0f8114bb6e
macM1 <zdai257@gmail.com>
2023-12-10 17:48:05 +0000

    Attempt to eval 'tiny' visual encoder

--- configuration.py
+++ configuration.py
@@ -11,16 +11,16 @@ class Config(object):
         self.weight_decay = 1e-4
 
         # Backbone
-        self.backbone = 'resnet50'
+        self.backbone = 'resnet18'
         self.position_embedding = 'sine'
         self.dilation = True
 
         # Basic
-        self.device = 'cuda:0'
+        self.device = 'cuda:1'
         self.seed = 42
         self.batch_size = 32
         self.num_workers = 8
-        self.checkpoint = './checkpoint-small.pth'
+        self.checkpoint = './checkpoint-tiny.pth'
         self.clip_max_norm = 0.1
 
         # Transformer
